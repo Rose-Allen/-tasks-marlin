9a5e7cecf210f3ae0b8609e51b6fdda3dc1a2cc2
Rose-Allen <77355864+Rose-Allen@users.noreply.github.com>
2021-12-12 16:32:10 +0600

    1
    1

---
+++ item.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$items = [
+    [
+        'title'=>'Reports',
+        'tag'=> 'reports file'
+    ],
+    [
+        'title'=>'Analytics',
+        'tag'=> 'analytics graphs'
+    ],
+    [
+        'title'=>'Export',
+        'tag'=>'export download'
+    ],
+    [
+        'title'=> 'Storage',
+        'tag'=> 'storage'
+    ]
+
+];
